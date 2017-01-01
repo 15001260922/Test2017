3096e1bb45446e15d4a62eff578ab5c5573029b2
TT <tt>
2017-01-01 22:54:56 +0800

    B本地5

--- app/src/main/java/com/tt/test2017/MainActivity.java
+++ app/src/main/java/com/tt/test2017/MainActivity.java
@@ -11,7 +11,7 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         System.out.println("first213123");
         System.out.println("gai");
-        System.out.println("A 本地3");
+        System.out.println("A 本地33");
 
     }
 }
